800696b7e8b596b4879c70781af02d2883c03ce5
Daniel Kuglitsch <Daniel.Kuglitsch@gmail.com>
2020-02-13 10:32:50 +0100

    Add project files.

---
+++ WpfApp3/ItemViewModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+   public class ViewModel
+        : INotifyPropertyChanged
+            , IDataErrorInfo
+    {
+        private readonly Dictionary<string, IList<string>> _validationErrors = new Dictionary<string, IList<string>>();
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(propertyName))
+                    return Error;
+
+                if (_validationErrors.ContainsKey(propertyName))
+                    return String.Join(Environment.NewLine, _validationErrors[propertyName]);
+
+                return String.Empty;
+            }
+        }
+
+        public string Error => String.Join(Environment.NewLine, GetAllErrors());
+
+        private IEnumerable<string> GetAllErrors()
+        {
+            return _validationErrors.SelectMany(kvp => kvp.Value).Where(e => !String.IsNullOrEmpty(e));
+        }
+
+        public void AddValidationError(string propertyName, string errorMessage)
+        {
+            if (!_validationErrors.ContainsKey(propertyName))
+                _validationErrors.Add(propertyName, new List<string>());
+
+            _validationErrors[propertyName].Add(errorMessage);
+        }
+
+        public void ClearValidationErrors(string propertyName)
+        {
+            if (_validationErrors.ContainsKey(propertyName))
+                _validationErrors.Remove(propertyName);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    public class ItemViewModel
+        : ViewModel
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+
+                    ClearValidationErrors(nameof(Name));
+
+                    if (String.IsNullOrEmpty(value))
+                        AddValidationError(nameof(Name), "Name must not be null or empty.");
+
+                    RaisePropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private double _weight;
+
+        public double Weight
+        {
+            get => _weight;
+            set
+            {
+                if (_weight != value)
+                {
+                    _weight = value;
+
+                    RaisePropertyChanged(nameof(Weight));
+                }
+            }
+        }
+
+        private bool _flag;
+        public bool Flag
+        {
+            get => _flag;
+            set
+            {
+                if (_flag != value)
+                {
+                    _flag = value;
+
+                    RaisePropertyChanged(nameof(Flag));
+                }
+            }
+        }
+
+        private readonly ObservableCollection<ItemViewModel> _children = new ObservableCollection<ItemViewModel>();
+
+        public ReadOnlyObservableCollection<ItemViewModel> Children { get; set; }
+
+        public ItemViewModel()
+        {
+            Children = new ReadOnlyObservableCollection<ItemViewModel>(_children);
+        }
+
+        public void AddChild(ItemViewModel child)
+        {
+            _children.Add(child);
+        }
+    }
+}
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<ItemViewModel> _items = new ObservableCollection<ItemViewModel>();
+        public ReadOnlyObservableCollection<ItemViewModel> Items { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Items = new ReadOnlyObservableCollection<ItemViewModel>(_items);
+
+            AddDummyItems(50);
+
+            tvitems.ItemsSource = Items;
+        }
+
+        private void AddDummyItems(int count)
+        {
+            foreach (ItemViewModel item in CreateDummyItems(count, 0.25, new Random()))
+            {
+                _items.Add(item);
+            }
+        }
+
+        private IEnumerable<ItemViewModel> CreateDummyItems(int count, double childCreationProbability, Random random)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var item = new ItemViewModel
+                {
+                    Name = CreateItemName(i + 1),
+                    Weight = random.NextDouble(),
+                };
+
+                if (random.NextDouble() <= childCreationProbability)
+                {
+                    foreach (ItemViewModel child in CreateDummyItems(random.Next(count), childCreationProbability, random))
+                    {
+                        item.AddChild(child);
+                    }
+                }
+
+                yield return item;
+            }
+        }
+
+        public ItemViewModel CreateItemInItems()
+        {
+            var newItem = new ItemViewModel
+            {
+                Name = GetNextUniqueItemName(),
+                Weight = new Random().NextDouble(),
+            };
+
+            _items.Add(newItem);
+            return newItem;
+        }
+
+        private string GetNextUniqueItemName()
+        {
+            int iteration = 1;
+            string itemName = CreateItemName(iteration);
+
+            while (_items.Any(item => item.Name == itemName))
+            {
+                iteration++;
+                itemName = CreateItemName(iteration);
+            }
+
+            return itemName;
+        }
+
+        private string CreateItemName(int itemId)
+        {
+            return $"Item {itemId}";
+        }
+
+    }
+}
